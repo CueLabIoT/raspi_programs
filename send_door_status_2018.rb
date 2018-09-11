79b16dba02f7ceb1912a224fe27a3b4c1a41e83d
zashiperm <l05109@shibaura-it.ac.jp>
2018-09-11 18:50:36 +0900

    set api-ver. toilets programs.

---
+++ send_door_status_2018.rb
@@ -0,0 +1,84 @@
+require 'net/http'
+require 'uri'
+require 'json'
+require 'pi_piper'
+
+include PiPiper
+
+fnum = "07"
+preTime1 = Time.now
+preTime2 = Time.now
+
+pin1 = Pin.new pin:18, direction: :in, pull: :down
+pin2 = Pin.new pin:7, direction: :in, pull: :down
+
+loop do
+	pin1.read
+   	 if pin1.changed? then
+    	 
+    	 p "pin 18 is pushed!!!!"
+
+	puts "show status"
+
+	now_time1 = Time.now
+	datetime_string = now_time1.strftime("%Y/%m/%d %H:%M:%S")
+
+	uri = URI.parse("https://o9q636x114.execute-api.ap-northeast-2.amazonaws.com/toilet")
+        response = nil
+
+        request = Net::HTTPS::Post.new(uri.request_uri, initheader = {'Content-Type' =>'application/json'})
+
+        https = Net::HTTPS.new(uri.host, uri.port)
+        https.set_debug_output $stderr
+
+        sdata = {
+        :type => "toiret",
+        :toiret_floor => fnum ,
+        :toiret_num => "0" ,
+        :used_time => now_time1 - preTime1,
+        :flag => pin1.value,
+        :datetime => datetime_string
+        }.to_json
+        request.body = sdata
+ 	 preTime1 = now_time1
+
+        http.start do |h|
+          response = https.request(request)
+        end
+        end
+
+	pin2.read
+   	 if pin2.changed? then
+
+    	 p "pin 7 is pushed!!!!"
+
+	puts "show status"
+
+	now_time2 = Time.now
+	datetime_string = now_time2.strftime("%Y/%m/%d %H:%M:%S")
+
+        uri = URI.parse("http://52.197.229.83/server_post")
+        response = nil
+
+        request = Net::HTTP::Post.new(uri.request_uri, initheader = {'Content-Type' =>'application/json'})
+
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.set_debug_output $stderr
+
+        sdata = {
+        :type => "toiret",
+        :toiret_floor => fnum ,
+        :toiret_num => "1" ,
+        :used_time => now_time2 - preTime2,
+        :flag => pin2.value,
+        :datetime => datetime_string
+        }.to_json
+        request.body = sdata
+ 	 preTime2 = now_time2
+
+        http.start do |h|
+          response = http.request(request)
+	 end
+	end
+ sleep 1.0
+end
