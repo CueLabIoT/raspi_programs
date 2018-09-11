9d96651951830eb3acdf4c820728be8a159ce44b
zashiperm <l05109@sic.shibaura-it.ac.jp>
2018-09-11 19:33:51 +0900

    add api-request.

--- send_door_status_2018.rb
+++ send_door_status_2018.rb
@@ -1,11 +1,11 @@
-require 'net/http'
+require 'net/https'
 require 'uri'
 require 'json'
 require 'pi_piper'
 
 include PiPiper
 
-fnum = "07"
+fnum = "06"
 preTime1 = Time.now
 preTime2 = Time.now
 
@@ -23,12 +23,21 @@ loop do
 	now_time1 = Time.now
 	datetime_string = now_time1.strftime("%Y/%m/%d %H:%M:%S")
 
-	uri = URI.parse("https://o9q636x114.execute-api.ap-northeast-2.amazonaws.com/toilet")
-        response = nil
+        uri = URI.parse("http://52.197.229.83/server_post") 
+	uri_api = URI.parse("https://o9q636x114.execute-api.ap-northeast-2.amazonaws.com/toilet/toiletinfos")
+        
+	response = nil
+        
+	request = Net::HTTP::Post.new(uri.request_uri, initheader = {'Content-Type' =>'application/json'})
 
-        request = Net::HTTPS::Post.new(uri.request_uri, initheader = {'Content-Type' =>'application/json'})
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.set_debug_output $stderr
+        
+	request_api = Net::HTTP::Put.new(uri_api.request_uri, initheader = {'Content-Type' =>'application/json'})
 
-        https = Net::HTTPS.new(uri.host, uri.port)
+        https = Net::HTTP.new(uri_api.host, uri_api.port)
+	https.use_ssl = true
+	https.verify_mode = OpenSSL::SSL::VERIFY_NONE
         https.set_debug_output $stderr
 
         sdata = {
@@ -40,12 +49,27 @@ loop do
         :datetime => datetime_string
         }.to_json
         request.body = sdata
- 	 preTime1 = now_time1
+        
+	sdata_api = {
+        :type => "toiret",
+        :toilet_floor => fnum ,
+        :toilet_num => "0" ,
+        :used_time => now_time1 - preTime1,
+        :flag => pin1.value,
+        :datetime => datetime_string
+        }.to_json
+        request_api.body = sdata_api
+ 	
+	 preTime1 = now_time1
 
         http.start do |h|
-          response = https.request(request)
+          response = http.request(request)
         end
+        https.start do |h|
+          response = https.request(request_api)
         end
+        
+	end
 
 	pin2.read
    	 if pin2.changed? then
@@ -57,13 +81,22 @@ loop do
 	now_time2 = Time.now
 	datetime_string = now_time2.strftime("%Y/%m/%d %H:%M:%S")
 
-        uri = URI.parse("http://52.197.229.83/server_post")
-        response = nil
+        uri = URI.parse("http://52.197.229.83/server_post") 
+	uri_api = URI.parse("https://o9q636x114.execute-api.ap-northeast-2.amazonaws.com/toilet/toiletinfos")
+
+	response = nil
 
         request = Net::HTTP::Post.new(uri.request_uri, initheader = {'Content-Type' =>'application/json'})
 
         http = Net::HTTP.new(uri.host, uri.port)
         http.set_debug_output $stderr
+
+        request_api = Net::HTTP::Put.new(uri_api.request_uri, initheader = {'Content-Type' =>'application/json'})
+
+        https = Net::HTTP.new(uri_api.host, uri_api.port)
+	https.use_ssl = true
+	https.verify_mode = OpenSSL::SSL::VERIFY_NONE
+        https.set_debug_output $stderr
 
         sdata = {
         :type => "toiret",
@@ -73,12 +106,29 @@ loop do
         :flag => pin2.value,
         :datetime => datetime_string
         }.to_json
-        request.body = sdata
- 	 preTime2 = now_time2
+
+        sdata_api = {
+        :type => "toiret",
+        :toilet_floor => fnum ,
+        :toilet_num => "1" ,
+        :used_time => now_time2 - preTime2,
+        :flag => pin2.value,
+        :datetime => datetime_string
+        }.to_json
+        
+	request.body = sdata
+	request_api.body = sdata_api
+ 	
+	preTime2 = now_time2
 
         http.start do |h|
           response = http.request(request)
 	 end
+
+        https.start do |h|
+          response = https.request(request_api)
+	 end
+
 	end
  sleep 1.0
 end
